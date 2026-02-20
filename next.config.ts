dd37f71d3ed398c34379159d8901f99b7b816a5b
Drew T Ernst <drew.t.ernst@gmail.com>
2026-02-19 18:36:07 -0600

    Fix next.config.ts conflict to enable static export for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,11 @@
 import type { NextConfig } from "next";
 
-module.exports = {
-  images: {
-    qualities: [80],
-  },
-};
 const nextConfig: NextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  reactCompiler: true,
+  // reactCompiler: true,
 };
 
 export default nextConfig;
